fc8b5f46da1c41355879c998109d42a49f02f2fb
XingJingxuan <xjx524@gmail.com>
2015-08-25 15:49:14 +0800

    update jobs page

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.dept').on('click', 'a', function(e) {
+    var ul = $($(this).data('parent'));
+    ul.find('a').each(function() {
+        $(this).removeClass('active');
+        $($(this).data('target')).addClass('hide');
+    });
+    $(this).addClass('active');
+    $($(this).data('target')).removeClass('hide')
+});
