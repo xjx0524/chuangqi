9c703a1a2b0f33546b3e72cd9be71e02efa3fe5d
XingJingxuan <xjx524@gmail.com>
2015-08-29 01:41:18 +0800

    responsive jobs page

--- js/main.js
+++ js/main.js
@@ -7,3 +7,13 @@ $('.dept').on('mouseenter', 'a', function(e) {
     $(this).addClass('active');
     $($(this).data('target')).removeClass('hide')
 });
+
+$('.dept-phone').on('click', 'a', function(e) {
+    var btn_group = $($(this).data('parent'));
+    btn_group.find('a').each(function() {
+        $(this).removeClass('active');
+        $($(this).data('target')).addClass('hide');
+    });
+    $(this).addClass('active');
+    $($(this).data('target')).removeClass('hide')
+});
